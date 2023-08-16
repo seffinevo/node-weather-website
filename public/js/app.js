82c9f3213b25ae9d2eb9bac626062f19857a3480
Seffi Nevo <seffinevo1@gmail.com>
2023-08-16 11:14:26 +0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log("client side js file is loaded")
+
+
+
+
+const weatherForm = document.querySelector('form')
+const searchEl = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault() // prevent browser from refreshing the page upon submiting
+
+    const location = searchEl.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return messageOne.textContent = data.error
+            }
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,30 @@
+const request = require('postman-request');
+
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=83ca3828472391608b08bfa45b7335b0&query=' + latitude + ',' + longitude;
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather services', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            //callback(undefined, {
+            //    temperature: body.current.temperature,
+            //    humidity: body.current.humidity,
+            //})
+            
+            const temperature = body.current.temperature;
+            const humidity = body.current.humidity;
+            const weatherDescription = body.current.weather_descriptions[0];
+            const location = body.location.name;
+            const country = body.location.country; 
+            callback(undefined, `${weatherDescription}. It is now ${temperature} degrees outside in ${location}, ${country}. Humidity is ${humidity} percent.`)
+        }
+    })
+}
+
+
+
+module.exports = forecast;
