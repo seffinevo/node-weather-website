ef1b210069442e4b91fffd69384ad5ef71088b0d
Seffi Nevo <seffinevo1@gmail.com>
2023-08-21 17:06:17 +0300

    added port

--- src/app.js
+++ src/app.js
@@ -103,6 +103,8 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
